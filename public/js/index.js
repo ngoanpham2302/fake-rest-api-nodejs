0a9ba447cd31a1f5fe891bb7d1e8f6a038eba287
ngoanpham2302 <ngoanptp2302@gmail.com>
2021-06-08 22:02:58 +0700

    modified index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,24 @@
+let currentPage = 1;
+let sortQuery = "_sort=id&_order=desc";
+let pageQuery = "_page=" + currentPage + "&_limit=2";
+let searchValue = "";
+let searchQuery = "q=" + searchValue;
+let pageLimitUsers = 2;
+let maxPages = 5;
+let pageDiff;
+
+function getUsersFail() {
+  renderAllUsers([]);
+  $(".table-responsive").children("p").remove();
+  $(".table-responsive").append(
+    "<p class='text-center text-danger'>Không tìm thấy học viên!</p>"
+  );
+}
+
 // Ready function
 $(function () {
-  getAllUsersApi();
+  getUsersByPageApi(currentPage);
 });
-
-function getAllUsersApi() {
-  $.ajax({
-    method: "GET",
-    url: "/users",
-  })
-    .done(function (users) {
-      renderAllUsers(users);
-    })
-    .fail(function () {
-      renderAllUsers([]);
-      $(".table-responsive").append(
-        "<p class='text-center text-danger'>Không tìm thấy học viên!</p>"
-      );
-    });
-}
 
 // Render thông tin tất cả users ra bảng
 function renderAllUsers(userArr) {
@@ -81,4 +82,176 @@ function removeUser(id) {
 // Button confirm remove (modal)
 $(".btn-confirm").click(function () {
   removeUserApi(userId, `.btn-remove-${userId}`);
+});
+
+//******************************//
+
+// Pagination
+function getUsersByPageApi(pageNumber) {
+  pageQuery = "_page=" + pageNumber + "&_limit=2";
+
+  $.ajax({
+    method: "GET",
+    url: "/users?" + sortQuery + "&" + pageQuery + "&" + searchQuery,
+  })
+    .done(function (pageUsers) {
+      renderAllUsers(pageUsers);
+      changePrevNextStatus();
+    })
+    .fail(function () {
+      getUsersFail();
+    });
+}
+
+// Page number button
+$(".page-number").click(function () {
+  $(".page-number").removeClass("active");
+  $(this).addClass("active");
+
+  currentPage = Number($(this).children().text());
+  getUsersByPageApi(currentPage);
+});
+
+// Page prev button
+$(".prev-page").click(function () {
+  if (currentPage > 1) {
+    currentPage--;
+  } else {
+    currentPage = 1;
+  }
+
+  $(".page-number").removeClass("active");
+  $($(".page-number")[currentPage - 1]).addClass("active");
+  getUsersByPageApi(currentPage);
+});
+
+// Page next button
+$(".next-page").click(function () {
+  if (currentPage < maxPages) {
+    currentPage++;
+  } else {
+    currentPage = maxPages;
+  }
+
+  $(".page-number").removeClass("active");
+  $($(".page-number")[currentPage - 1]).addClass("active");
+  getUsersByPageApi(currentPage);
+});
+
+// Add/remove class active for page prev/next button
+function changePrevNextStatus() {
+  if (currentPage > 1) {
+    $(".prev-page").removeClass("disabled-btn");
+  } else {
+    $(".prev-page").addClass("disabled-btn");
+  }
+
+  if (currentPage < maxPages) {
+    $(".next-page").removeClass("disabled-btn");
+  } else {
+    $(".next-page").addClass("disabled-btn");
+  }
+}
+
+//******************************//
+
+// Sort users by properties
+let isAsc = false;
+
+function getUsersBySortApi(sortQuery) {
+  $.ajax({
+    method: "GET",
+    url: "/users?" + sortQuery + "&" + pageQuery + "&" + searchQuery,
+  })
+    .done(function (users) {
+      renderAllUsers(users);
+    })
+    .fail(function () {
+      getUsersFail();
+    });
+}
+
+// Table header - click to sort
+$(".table-header").click(sortUsers);
+
+function sortUsers() {
+  setSortStyle(this);
+  let dataSort = $(this).attr("data-sort");
+
+  if (!isAsc) {
+    if (dataSort == "name") {
+      sortQuery = getSortQuery("firstName,lastName", "asc,asc");
+    } else {
+      sortQuery = getSortQuery(dataSort, "asc");
+    }
+
+    showSortupIcon(this);
+    isAsc = true;
+  } else {
+    if (dataSort == "name") {
+      sortQuery = getSortQuery("firstName,lastName", "desc,desc");
+    } else {
+      sortQuery = getSortQuery(dataSort, "desc");
+    }
+
+    showSortDownIcon(this);
+    isAsc = false;
+  }
+
+  getUsersBySortApi(sortQuery);
+}
+
+// Get sort query
+function getSortQuery(sort, order) {
+  sortParams = "_sort=" + sort + "&_order=" + order;
+  return sortParams;
+}
+
+// Set CSS style for sort icon
+function setSortStyle(element) {
+  $(".table-header").not(element).find(".sortdown-btn").addClass("d-none");
+  $(".table-header").not(element).find(".sortup-btn").addClass("d-none");
+  $(".table-header").not(element).find(".sort-btn").removeClass("d-none");
+}
+
+function showSortupIcon(element) {
+  $(element).find(".sort-btn").addClass("d-none");
+  $(element).find(".sortdown-btn").addClass("d-none");
+  $(element).find(".sortup-btn").removeClass("d-none");
+}
+
+function showSortDownIcon(element) {
+  $(element).find(".sort-btn").addClass("d-none");
+  $(element).find(".sortup-btn").addClass("d-none");
+  $(element).find(".sortdown-btn").removeClass("d-none");
+}
+
+//******************************//
+
+// Search for users
+function getUsersBySearchApi(searchQuery) {
+  $.ajax({
+    method: "GET",
+    url: "/users?" + sortQuery + "&" + pageQuery + "&" + searchQuery,
+  })
+    .done(function (users) {
+      renderAllUsers(users);
+    })
+    .fail(function () {
+      getUsersFail();
+    });
+}
+
+// Search input event listener
+$("#search-input").keyup(function () {
+  searchValue = $("#search-input").val();
+  searchQuery = "q=" + searchValue;
+
+  // Take back to page 1, update pageQuery
+  currentPage = 1;
+  pageQuery = "_page=1&_limit=2";
+  $(".page-number").removeClass("active");
+  $($(".page-number")[0]).addClass("active");
+
+  getUsersBySearchApi(searchQuery);
 });
